d852ee5b704462d2a8493026ad1f560e14eccded
Will Greenberg <will@endlessm.com>
2014-05-19 18:02:48 -0700

    Routed Server and the main file
    RoutedServer is a Soup Server which uses the Router to mediate requests
    
    xapianBridge.js has a main function which sets up the routes for the
    bridge API specified in the backend architecture doc
    
    [endlessm/eos-sdk#1055]

---
+++ src/xapianBridge.js
@@ -0,0 +1,35 @@
+const Soup = imports.gi.Soup;
+
+const RoutedServer = imports.routedServer.RoutedServer;
+
+const DEFAULT_PORT = 5000;
+
+function main () {
+    let server = new RoutedServer({
+        port: DEFAULT_PORT
+    });
+
+    // PUT /:indexName - add/update the xapian database at indexName
+    server.put('/:indexName', function (params, query, msg) {
+        let indexName = params.indexName;
+        let path = query.path;
+        // TODO: prepare/update a xapian database at path
+    });
+
+    // DELETE /:indexName - remove the xapian database at indexName
+    server.delete('/:indexName', function (params, query, msg) {
+        let indexName = params.indexName;
+        // TODO: delete the xapian server at indexName
+    });
+
+    // GET /:indexName/query - query an index
+    server.get('/:indexName/query', function (params, query, msg) {
+        let indexName = params.indexName;
+        let q = query.q;
+        let collapse = query.collapse;
+        let limit = query.limit;
+        // TODO: query the xapian database at indexName, respond with json results
+    });
+
+    server.run();
+}
