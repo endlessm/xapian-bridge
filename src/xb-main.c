7480c126e4d165cd54053997324957f542ca1917
Cosimo Cecchi <cosimo@endlessm.com>
2016-07-13 11:32:47 -0700

    Revert "flatpak: remove unused function parameter"
    This reverts commit 6ba20ed47f5914a6a245544a344f94881c63cf19.
    
    https://phabricator.endlessm.com/T11523

--- src/xb-main.c
+++ src/xb-main.c
@@ -89,8 +89,9 @@ server_send_response (SoupMessage *message,
 }
 
 static char *
-resolve_flatpak_path (const char *path,
-                      const char *app_id)
+get_real_path (const char *path,
+               const char *app_id,
+               gboolean for_subscriptions)
 {
   g_autofree char *real_path = g_file_read_link (path, NULL);
   if (!real_path)
@@ -150,9 +151,9 @@ fill_xbdb_from_query (SoupMessage *message,
       if (app_id != NULL)
         {
           if (path != NULL)
-            db->path = resolve_flatpak_path (path, app_id);
+            db->path = get_real_path (path, app_id, FALSE);
           if (manifest_path != NULL)
-            db->manifest_path = resolve_flatpak_path (manifest_path, app_id);
+            db->manifest_path = get_real_path (manifest_path, app_id, TRUE);
         }
     }
 
